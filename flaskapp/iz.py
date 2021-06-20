232d567041f4ddf372cd92c877152e1f470f1883
Jarik23 <85994092+Jarik23@users.noreply.github.com>
2021-06-20 14:36:03 +0700

    Update iz.py

--- flaskapp/iz.py
+++ flaskapp/iz.py
@@ -1,50 +1,41 @@
-print("Hello world")
 from flask import Flask
 app = Flask(__name__)
-#декоратор для вывода страницы по умолчанию
+
 @app.route("/")
 def hello():
- return " <html><head></head> <body> Hello World! </body></html>"
+ return " <html><head></head> <body> Enter </body></html>"
 
 from flask import render_template
-#наша новая функция сайта
 
-# модули работы с формами и полями в формах
 from flask_wtf import FlaskForm,RecaptchaField
 from wtforms import StringField, SubmitField, TextAreaField
-# модули валидации полей формы
+
 from wtforms.validators import DataRequired
 from flask_wtf.file import FileField, FileAllowed, FileRequired
-# используем csrf токен, можете генерировать его сами
+
 SECRET_KEY = 'secret'
 app.config['SECRET_KEY'] = SECRET_KEY
-# используем капчу и полученные секретные ключи с сайта google 
+
 app.config['RECAPTCHA_USE_SSL'] = False
-app.config['RECAPTCHA_PUBLIC_KEY'] = '6LecVzgbAAAAAKRNfVuiHYRQGAhfb2VUZ2h8xU1v'
-app.config['RECAPTCHA_PRIVATE_KEY'] = '6LecVzgbAAAAADBFkz1pDO4uLq_QD1lb5lEP2jxX'
+app.config['RECAPTCHA_PUBLIC_KEY'] = '6Len7C0bAAAAAP2x8aRDGy57sEiyn6_psJSDDHzo'
+app.config['RECAPTCHA_PRIVATE_KEY'] = '6Len7C0bAAAAAEhAW7vclLdDveHmz-DXRBTGK1zr'
 app.config['RECAPTCHA_OPTIONS'] = {'theme': 'white'}
-# обязательно добавить для работы со стандартными шаблонами
+
 from flask_bootstrap import Bootstrap
 bootstrap = Bootstrap(app)
-# создаем форму для загрузки файла
+
 class NetForm(FlaskForm):
- # поле для введения строки, валидируется наличием данных
- # валидатор проверяет введение данных после нажатия кнопки submit
- # и указывает пользователю ввести данные если они не введены
- # или неверны
- cho = StringField('Поворот изображения', validators = [DataRequired()])
- # поле загрузки файла
- # здесь валидатор укажет ввести правильные файлы
+
+ cho = StringField('Введите угол поворота', validators = [DataRequired()])
+
  upload = FileField('Load image', validators=[
  FileRequired(),
  FileAllowed(['jpg', 'png', 'jpeg'], 'Images only!')])
- # поле формы с capture
+
  recaptcha = RecaptchaField()
- #кнопка submit, для пользователя отображена как send
+
  submit = SubmitField('send')
-# функция обработки запросов на адрес 127.0.0.1:5000/net
-# модуль проверки и преобразование имени файла
-# для устранения в имени символов типа / и т.д.
+
 from werkzeug.utils import secure_filename
 import os
 
@@ -53,15 +44,13 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-## функция для оброботки изображения 
+
 def draw(filename,cho):
- ##открываем изображение 
  print(filename)
  img= Image.open(filename)
  x, y = img.size
  cho=int(cho)
- 
-##делаем график
+ ##gra
  fig = plt.figure(figsize=(6, 4))
  ax = fig.add_subplot()
  data = np.random.randint(0, 255, (100, 100))
@@ -70,42 +59,34 @@ def draw(filename,cho):
  fig.colorbar(b, ax=ax)
  gr_path = "./static/newgr.png"
  sns.displot(data)
- #plt.show()
  plt.savefig(gr_path)
  plt.close()
-
-
-##меняем половинки
-  cho==DataRequired(): 
-  
-  img_rot = interp.rotate(input=image_copy, angle=cho , reshape = False)
-  img_rot_r = interp.rotate(input=image_copy, angle=cho , reshape = True)
-  output_filename = filename
-  img.save(output_filename)
-  return output_filename,gr_path
  
+ import scipy.ndimage.interpolation as interp
+ img = interp.rotate(input=img, angle=cho, axes=(0,1), reshape = False)
+ 
+ #img=img.rotate(cho)
+ output_filename = filename
+ img.save(output_filename)
+return output_filename,gr_path
 
-
-
-# метод обработки запроса GET и POST от клиента
 @app.route("/net",methods=['GET', 'POST'])
 def net():
- # создаем объект формы
+
  form = NetForm()
- # обнуляем переменные передаваемые в форму
+
  filename=None
  newfilename=None
  grname=None
- # проверяем нажатие сабмит и валидацию введенных данных
+
  if form.validate_on_submit():
-  # файлы с изображениями читаются из каталога static
+
   filename = os.path.join('./static', secure_filename(form.upload.data.filename))
   ch=form.cho.data
  
   form.upload.data.save(filename)
   newfilename,grname = draw(filename,ch)
- # передаем форму в шаблон, так же передаем имя файла и результат работы нейронной
- # сети если был нажат сабмит, либо передадим falsy значения
+
  
  return render_template('net.html',form=form,image_name=newfilename,gr_name=grname)
 
